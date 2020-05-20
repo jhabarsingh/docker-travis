9a43c84326f196fd9c6f953fb00c8c1ef1fe6e6c
jhabar <jhabarsinghbhati23@gmail.com>
2020-05-20 06:33:17 +0530

    tedtcases added

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class ModelTests(TestCase):
+
+	def trust_create_user_with_email_successful(self):
+		"""Test creating a new user with email is successful"""
+		email = "test@email.com"
+		password = "Jhabar@123"
+		user = get_user_model().objects.create(
+				email = email,
+				password = password
+			)	
+
+		self.assertEqual(user.email, email)
+		self.assertTrue(user.check_password(password))
+
+	
